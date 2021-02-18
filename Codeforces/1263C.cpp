b06712a9ee68981f8d15244c47fd2a1ebffe8688
Michael Bao <68862733+tcmmichaelb139@users.noreply.github.com>
2021-02-18 17:48:56 -0500

    Create 1263C.cpp

---
+++ Codeforces/1263C.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<double, double> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+
+#define vc vector
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define sz size
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define ub upper_bound
+#define lb lower_bound
+
+#define f first
+#define s second
+
+#define ln length
+
+void IO(string s = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    if (s.length()) {
+        freopen((s+".in").c_str(),"r",stdin);
+        freopen((s+".out").c_str(),"w",stdout);
+    }
+}
+
+void solve() {
+    int k;
+    cin >> k;
+    vi v;
+    int s = (int)sqrt(k);
+    for (int i = 0; i <= s; i++) {
+        v.pb(i);
+    }
+    for (int i = 1; i <= s; i++) {
+        v.pb(k/i);
+    }
+    sort(all(v));
+    v.rsz(unique(all(v))-v.begin());
+    cout << v.size() << '\n';
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    IO("");
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        solve();
+    }
+}
