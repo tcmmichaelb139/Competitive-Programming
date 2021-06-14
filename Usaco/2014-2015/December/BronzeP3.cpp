9a0cf9288a9eb6ae2bc6345d94a5414a428421a6
Michael Bao <68862733+tcmmichaelb139@users.noreply.github.com>
2021-06-14 13:25:45 -0400

    Create BronzeP3.cpp

---
+++ Usaco/2014-2015/December/BronzeP3.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<double, double> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+
+#define vc vector
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define sz size
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define ub upper_bound
+#define lb lower_bound
+
+#define f first
+#define s second
+#define mp make_pair
+
+#define ln length
+#define IMX INT_MAX
+#define IMN INT_MIN
+
+void IO(string s = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    if (s.length()) {
+        freopen((s+".in").c_str(),"r",stdin);
+        freopen((s+".out").c_str(),"w",stdout);
+    }
+}
+
+int main() {
+    IO("");
+
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        cin >> v[i];
+    }
+    int prev;
+    int ans = 1;
+    for (int i = n-1; i >= 0; i--) {
+        if (i == n-1) {
+            prev = v[i];
+            continue;
+        }
+        if (prev >= v[i]) {
+            ans++;
+            prev = v[i];
+        }
+    }
+    cout << ans << '\n';
+}
+
+/* Stuff to look for
+ * int overflow, array bounds
+ * special cases (n=1)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ * GET RID OF DEBUGGING CODE BEFORE SUBMITTING
+ */
