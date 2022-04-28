ca525256deac8c42986a8504e0ee8cf722c7a680
Michael <tcmb139@Michaels-MacBook-Pro.local>
2022-04-28 11:27:28 -0400

    update

--- template.cpp
+++ template.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); 
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 }
-
-/* Stuff to look for
- * int overflow, array bounds
- * special cases (n=1)
- * do smth instead of nothing and stay organized
- * WRITE STUFF DOWN
- * DON'T GET STUCK ON ONE APPROACH
- */
